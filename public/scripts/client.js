ee1f7e8cdb7c7e22645b69d7dbcd27be36bcbcdf
Fred <frederique.bordeleau@gmail.com>
2020-01-31 01:14:27 +0000

    Clean css, post tweet without reloading page, manage new Tweet button

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,120 +2,153 @@ $(document).ready(function() {
 
   let createTweetElement = function(tweetData) {
   
-  //BOSS
-  let $mainDiv       = $('<div>').addClass('boxedTweet');
+    //BOSS
+    let $mainDiv       = $('<div>').addClass('boxedTweet');
 
 
-  //Children of BOSS
-  let $header        = $('<header>').addClass('userInformation');
-  let $par           = $('<p>').attr('id', 'tweetLine');
-                       $par.text(tweetData.content.text);
-  let $footer        = $('<footer>').addClass('postInfoAndIcons');
+    //Children of BOSS
+    let $header        = $('<header>').addClass('userInformation');
+    let $par           = $('<p>').attr('id', 'tweetLine');
+    $par.text(tweetData.content.text);
+    let $footer        = $('<footer>').addClass('postInfoAndIcons');
 
 
-            //Children of header
-            let $divAvatarInfo = $('<div>').attr('id', 'avatarInfo');
-            let $spanOfHandler = $('<span>').attr('id', 'handler');
-                                 $spanOfHandler.text(tweetData.user.handle);
+    //Children of header
+    let $divAvatarInfo = $('<div>').attr('id', 'avatarInfo');
+    let $spanOfHandler = $('<span>').attr('id', 'handler');
+    $spanOfHandler.text(tweetData.user.handle);
 
 
-                              //Children of divAvatarInfo
-                              let $imgOfAvatar = $('<img>');
-                                                 $imgOfAvatar.attr('src', tweetData.user.avatars)
-                              let $spanOfName  = $('<span>').attr('id', 'name');
-                                                 $spanOfName.text(tweetData.user.name)  
+    //Children of divAvatarInfo
+    let $imgOfAvatar = $('<img>');
+    $imgOfAvatar.attr('src', tweetData.user.avatars);
+    let $spanOfName  = $('<span>').attr('id', 'name');
+    $spanOfName.text(tweetData.user.name);
 
 
-           //Children of footer
-           let $spanTimePost   = $('<span>').attr('id', 'timeOfTweet');
-                                 $spanTimePost.text(tweetData.created_at)
-           let $divOfIcons     = $('<div>').attr('id', 'threeIcons');
-                                 //$divOfIcons empyt for now;
+    //Children of footer
+    let tweetMoment = moment(tweetData['created_at']).fromNow();
+    let $spanTimePost   = $('<span>').text(tweetMoment).attr('id', 'timeOfTweet');
+    let $divOfIcons     = $('<div>').attr('id', 'threeIcons');
+
+    //Children of divOfIcons
+    let $iconShare      = $('<i>').addClass('fas fa-share-square')
+    let $iconHeart      = $('<i>').addClass('fas fa-heart')
+    let $iconComment    = $('<i>').addClass('fas fa-comment')
 
   
   
-  // APPENDING FROM GRAND-CHILDREN TO BOSS
+    // APPENDING FROM GRAND-CHILDREN TO BOSS
 
+    $divOfIcons
+      .append($iconShare)
+      .append($iconHeart)
+      .append($iconComment);
+    
     $divAvatarInfo
-          .append($imgOfAvatar)
-          .append($spanOfName);
+      .append($imgOfAvatar)
+      .append($spanOfName);
     
     $header
-          .append($divAvatarInfo)
-          .append($spanOfHandler);
+      .append($divAvatarInfo)
+      .append($spanOfHandler);
     
     $footer
-          .append($spanTimePost)
-          .append($divOfIcons)
+      .append($spanTimePost)
+      .append($divOfIcons);
 
     $mainDiv
-          .append($header)
-          .append($par)
-          .append($footer);
+      .append($header)
+      .append($par)
+      .append($footer);
     
-  return  $mainDiv;
+    return  $mainDiv;
 
-};
-
-
-// MANAGING TWEET FEED|
-//____________________|
+  };
 
 
+  // MANAGING TWEET FEED|
+  //____________________|
 
-$( ".form" ).on( "submit", function(event) {
-  debugger;
-  if ($('#textArea').text < 0 && $('#textArea').text < 140) {
-    event.preventDefault();
-    $.ajax ({
-      method:'POST',
-      url: 'http://localhost:8080/tweets',
-      data: $('form').serialize() 
-    })
-  } else if ($('#textArea').text === 0) {
-        alert('You forgot to actually write something, weirdo..')
-  } else if ($('#textArea').text > 140 ) {
-        alert('Too much characters....aaaahrhrrhhrhajshf!!!!')
-  }
- 
-    //.done(createTweetElement)
-    //.fail(handleCommentLoadErrors);
-    //console.log( $( this ).serialize() );
-});
+  $(".actionAndButton").click(function() {
+    $(".new-tweet").slideToggle();
+  });
 
-const renderTweets = function(tweets) {
-    for(let eachTweet of tweets) {
-      let result = createTweetElement(eachTweet)
-      $('#tweetsContainer').append(result)
-    }
-};
-
-
-const loadTweets = function() {
+  const loadOneTweet = function() {
     $.ajax({
-        method: 'GET',
-        url: 'http://localhost:8080/tweets'
+      method: 'GET',
+      url: 'http://localhost:8080/tweets'
     })
-        .done(renderTweets);
-};
+      .done(function(data) {
+        let newTweet = createTweetElement(data.pop());
+        $('#tweetsContainer').prepend(newTweet);
+        $('#textArea').val("");
+        $('.counter').html(140);
+      });
+  };
+  loadOneTweet();
 
 
-loadTweets();
+  $(".form").on("submit", function(event) {
+    event.preventDefault();
+    if (!$('#textArea').val()) {
+      $("#errorNothing").removeClass("hidden");
+      $("#errorTooLong").addClass("hidden");
+    } else if ($('.counter').hasClass('counterUnderZero')) {
+      $("#errorTooLong").removeClass("hidden");
+      $("#errorNothing").addClass("hidden");
+    } else {
+      $("#errorTooLong").addClass("hidden");
+      $("#errorNothing").addClass("hidden");
+      $.ajax({
+        method:'POST',
+        url: 'http://localhost:8080/tweets',
+        data: $('form').serialize()
+      })
+        .done(loadOneTweet);
+    //.fail(handleCommentLoadErrors);
+    }
+  });
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+ 
+
+
+  const renderTweets = function(tweets) {
+    for (let eachTweet of tweets) {
+      let result = createTweetElement(eachTweet);
+      $('#tweetsContainer').prepend(result);
+    }
+  };
+
+
+
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:8080/tweets'
+    })
+      .done(renderTweets);
+  };
+
+  loadTweets();
+
+
+
+
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227,
+  };
 
-const $tweet = createTweetElement(tweetData);
-$('#tweetsContainer').append($tweet); 
+  const $tweet = createTweetElement(tweetData);
+  $('#tweetsContainer').prepend($tweet);
 });
 
 
